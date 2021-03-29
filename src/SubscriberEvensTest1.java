f31d5856c02b08fff25815720e08a05ee6c704a7
JsMailhot <47703965+JsMailhot@users.noreply.github.com>
2021-03-28 23:59:32 -0400

    Adding from master
    James added a new branch that somehow doesn't share the commit history

---
+++ src/SubscriberEvensTest1.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.jupiter.api.Test;
+
+class SubscriberEvensTest1 {
+
+	@Test
+	void test() {
+		PublisherImpl publisher = new PublisherImpl();
+		Boolean actual = true;
+		SubscriberEvensImpl subscirberEven = new SubscriberEvensImpl(publisher);
+		for (int i = 0; i < 22; i++) {
+			int int_random = ThreadLocalRandom.current().nextInt();
+			Event Event = new Event(int_random, i);
+		actual = subscirberEven.notfiySubscriber(Event);
+	}
+		assertEquals(false, actual);
+	}
+}
